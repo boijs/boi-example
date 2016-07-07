b8b098280e26703635e59bf9bddb5086f29149fc
zhoujunpeng <zjp0432@163.com>
2016-07-07 15:12:18 +0800

    add svg sprite demo

---
+++ svg-sprite/boi-conf.js
@@ -0,0 +1,47 @@
+boi.use('boi-plugin-loader-svgsprite',{
+    name: 'svg/svg-sprite.[contenthash:8].svg',
+    inject: false,
+    targets: ['index.svgtest.html']
+});
+
+// 配置
+boi.spec('basic', {
+    appName: 'svgtest',
+    // 本地编译目录
+    localPath: {
+        src: './src/',
+        dest: './dest/',
+        thirdparty: './libs/'
+    },
+    // cdn url
+    cdn: {
+        server: 'static.svgtest.com',
+        path: '/svgtest/'
+    }
+});
+
+// 同名配置覆盖父级
+boi.spec('js', {
+    extType: 'js',
+    srcType: ['es2015'],
+    srcDir: 'js',
+    destDir: 'js',
+    useHash: true
+});
+
+boi.spec('style', {
+    extType: 'scss',
+    srcDir: 'style',
+    destDir: 'style',
+    useHash: true
+});
+
+boi.spec('html', {
+    extType: 'html',
+    srcDir: 'views',
+});
+
+boi.spec('image', {
+    extType: ['png', 'jpg'],
+    destDir: 'image'
+});
---
+++ svg-sprite/src/js/main.svgtest.js
@@ -0,0 +1,5 @@
+'use strict'
+import '../styles/main.svgtest.scss';
+
+// import '../assets/svg/icon-beauty.svg';
+let svgs = require.context("../assets/svg/", false, /\.svg$/);
